81c53227f80e066cf5bf158b3162d233cb4cb214
luminxueba222 <3037000388@qq.com>
2019-08-10 10:38:52 +0800

    s

---
+++ src/views/common/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <div class="l">
+      <div>
+        <img src="../../assets/img/logo.png" alt />
+      </div>
+      <div>后台管理系统1</div>
+    </div>
+    <div class="r">
+      <span>
+        <span>name</span>|
+        <img src="../../assets/img/logo.png" alt />
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+}
+.l {
+  width: 220px;
+  background: #000;
+}
+.r {
+}
+</style>
