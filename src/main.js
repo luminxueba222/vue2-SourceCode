bc9f77399c2c6a4011abe019d47b6766a1c0fa88
luminxueba222 <3037000388@qq.com>
2019-08-14 22:22:26 +0800

    echras

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import Element from 'element-ui'
 import Axios from 'axios'
 import './mock/index'
 import 'element-ui/lib/theme-chalk/index.css'
+import echarts from 'echarts'
+import 'echarts-gl';
+import "echarts/lib/component/legend"
+Vue.prototype.echarts = echarts
 Vue.prototype.axios = Axios
 Vue.use(Element, {
   size: 'small'
--- src/views/page/Dashboard.vue
+++ src/views/page/Dashboard.vue
@@ -15,16 +15,15 @@
         </el-card>
         <div style="height:12px"></div>
         <el-card shadow="hover">
-          <div class="cardHeader">
-            <div class="imgbox">
-              <img src="../../assets/img/logo.png" alt />
-            </div>
-            <div class="user">
-              <div>admin</div>
-              <div class="role">超级管理员</div>
-            </div>
-          </div>
-        </el-card>
+  <div slot="header" class="clearfix">
+    <span>语言详情</span>
+  </div>
+  <div v-for="o in 4" :key="o" class="text item">
+    vue:
+    <el-progress :percentage="50"></el-progress>
+  </div>
+</el-card>
+           <div id="EC" ></div>
       </el-col>
       <el-col :span="16">
         <div class="webInfo">
@@ -115,6 +114,37 @@ export default {
         }
       ]
     };
+  },
+  mounted(){
+    this.init()
+  },
+  methods:{
+    init(){
+        var chart = this.echarts.init(document.getElementById('EC'));
+        console.log(chart,1111);
+         var option = {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data:['销量']
+            },
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        chart.setOption(option);
+
+    }
   }
 };
 </script>
@@ -155,7 +185,8 @@ export default {
   flex-direction: row;
   flex-grow: 1;
   background: #fff;
-  margin-left: 20px;
+  /* justify-content: space-between; */
+  /* margin-right: 20px; */
 }
 .iconBox {
   padding: 30px;
@@ -181,5 +212,9 @@ export default {
   /* background: #fff; */
   margin-bottom: 12px;
   display: flex;
+}
+#EC{
+  /* width: 500px;
+  height: 500px; */
 }
 </style>
