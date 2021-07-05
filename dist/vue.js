33314d2c999e658e1603a8b46825f290894eaf2a
xialinhui <xialinhui@shihuituan.com>
2021-07-05 19:09:03 +0800

    init

--- dist/vue.js
+++ dist/vue.js
@@ -96,7 +96,6 @@
 
   function initMixin(Vue) {
     Vue.prototype._init = function (options) {
-      console.log("options", options);
       var vm = this;
       vm.$options = options;
       initState(vm);
--- src/init.js
+++ src/init.js
@@ -2,7 +2,6 @@ import { initState } from "./state";
 
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
-    console.log("options", options);
     const vm = this;
     vm.$options = options;
     initState(vm);
